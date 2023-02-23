5a4ef2a3480063ad5f3ce076a3f2ba3ec96da964
Erika Zibelnik <ezibelnik@valorsystems.com>
2023-02-23 13:38:14 -0800

    login box and centering

--- quiz/src/components/Login.js
+++ quiz/src/components/Login.js
@@ -1,9 +1,48 @@
 import React from 'react'
+import { Button, Card, CardContent, TextField, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import Center from './Center'
 
 export default function Login() {
   return (
-    <div>
-      
-    </div>
+    <Center>
+      <Card sx={{
+        width: '400px',
+        textAlign: 'center'
+      }}>
+        <CardContent>
+          <Typography
+            variant='h3'
+            sx={{my: 3}}
+          >Quiz App</Typography>
+        <Box sx={{
+          '& .MuiTextField-root': {
+            m: '1',
+            width: '90%'
+          }
+        }}>
+          <form noValidate>
+            <TextField
+              label="Email"
+              name="email"
+              variant="outlined"
+            />
+            <TextField
+              label="Name"
+              name="name"
+              variant="outlined"
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              size="large"
+              sx={{ width: '90%' }}
+            >Start
+            </Button>
+          </form>
+        </Box>
+      </CardContent>
+    </Card>
+    </Center>
   )
 }
