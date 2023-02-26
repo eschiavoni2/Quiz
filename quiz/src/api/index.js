628e4d8584f38ddf7a45d0b3a6f06ce4a6980042
Erika Zibelnik <ezibelnik@valorsystems.com>
2023-02-26 13:31:17 -0800

    questions to appear on page and cycle through

--- quiz/src/api/index.js
+++ quiz/src/api/index.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 export const BASE_URL = 'http://localhost:5063/';
 
 export const ENDPOINTS = {
-  participant: 'participant'
+  participant: 'participant',
+  question: 'question'
 }
 
 export const createAPIEndpoint = endpoint => {
--- quiz/src/components/Quiz.js
+++ quiz/src/components/Quiz.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import useStateContext from '../hooks/useStateContext'
+import { Card, CardContent, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { createAPIEndpoint, ENDPOINTS } from '../api'
 
 export default function Quiz() {
-  const { context, setContext } = useStateContext();
-  console.log(context);
+  
+  const [qns, setQns] = useState([]);
+  const [qnIndex, setQnIndex] = useState(0);
+  
+  useEffect(() => {
+    createAPIEndpoint(ENDPOINTS.question)
+      .fetch()
+      .then(res => {
+        setQns(res.data)
+        console.log(res.data);
+      })
+    .catch(err=>{console.log(err);})
+  },[])
 
   return (
-    <div>
-      
-    </div>
+    qns.length !== 0
+      ?
+      <Card>
+        <CardContent>
+          <Typography variant="h6">
+            {qns[qnIndex].qnInWords}
+          </Typography>
+        </CardContent>
+      </Card>
+      :null
   )
 }
