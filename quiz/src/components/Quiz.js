144c59f294fad0710cbf1d8704d35f0720080ed7
Erika Zibelnik <ezibelnik@valorsystems.com>
2023-02-26 14:20:06 -0800

    answers to select, title, and timer

--- quiz/src/components/Quiz.js
+++ quiz/src/components/Quiz.js
@@ -1,4 +1,4 @@
-import { Card, CardContent, Typography } from '@mui/material'
+import { Card, CardContent, CardHeader, List, ListItemButton, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import { createAPIEndpoint, ENDPOINTS } from '../api'
 
@@ -6,6 +6,13 @@ export default function Quiz() {
   
   const [qns, setQns] = useState([]);
   const [qnIndex, setQnIndex] = useState(0);
+  const [time, setTime] = useState(0);
+
+  const startTimer = () => {
+    setInterval(() => {
+      setTime(prev => prev +1)
+    },[1000])
+  }
   
   useEffect(() => {
     createAPIEndpoint(ENDPOINTS.question)
@@ -20,11 +27,26 @@ export default function Quiz() {
   return (
     qns.length !== 0
       ?
-      <Card>
+      <Card
+        sx={{maxWidth: 640, mx: 'auto', mt: 5}}
+      >
+        <CardHeader title={"Question " + (qnIndex + 1) + " of 5"} />
         <CardContent>
           <Typography variant="h6">
             {qns[qnIndex].qnInWords}
           </Typography>
+          <List>
+            {qns[qnIndex].options.map((item, idx) =>
+              <ListItemButton
+                key={idx}
+                disableRipple>
+                <div>
+                  <b>{String.fromCharCode(65 + idx) + " . "}</b>{item}
+                </div>
+
+              </ListItemButton>
+            )}
+          </List>
         </CardContent>
       </Card>
       :null
